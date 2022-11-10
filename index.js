01e755116ede26e251bb7805f31948023b948d96
sarava338 <93024887+sarava338@users.noreply.github.com>
2022-11-10 20:47:02 +0530

    create index.js test.sh

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+    const url = new URL('http://localhost:8000' + req.url)
+
+    var q = url.search.split('&').map(e => e.split('=')).map(e => e[1])
+
+    switch (q[0]) {
+        case 'add':
+            res.end("Addition is: " + (Number(q[1]) + Number(q[2])))
+            break
+        case 'subtract':
+            res.end("Subtraction is " + (Number(q[1]) - Number(q[2])))
+            break
+        case 'multiple':
+            res.end("Multiplication is: " + (Number(q[1]) * Number(q[2])))
+            break
+        case 'divide':
+            res.end("Division is: " + (Number(q[1]) / Number(q[2])))
+            break
+        default:
+            res.end("Division is: " + (Number(q[1]) / Number(q[2])))
+    }
+
+}).listen(8000)
